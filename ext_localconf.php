ce3543c60ee6f305cc016ce149a2444562547d69
Alex Kellner <alexander.kellner@in2code.de>
2018-02-06 19:58:36 +0100

    [TASK] Small code cleanup

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,23 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
+    die('Access denied.');
 }
 
-call_user_func(function () {
+call_user_func(
+    function () {
 
-    /**
-     * Include Frontend Plugins for Powermail
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'In2code.ipandlanguageredirect',
-        'Pi1',
-        [
-            'Redirect' => 'redirect,suggest'
-        ],
-        [
-            'Redirect' => 'redirect'
-        ]
-    );
-});
+        /**
+         * Include Frontend Plugins
+         */
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'In2code.ipandlanguageredirect',
+            'Pi1',
+            [
+                'Redirect' => 'redirect,suggest'
+            ],
+            [
+                'Redirect' => 'redirect'
+            ]
+        );
+    }
+);
