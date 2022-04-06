4a040ca0cf14b6e9ffeb66a1a79edf1624dae7d1
Patrick Lenk <dev@avatara.de>
2022-04-06 11:12:01 +0200

    [!!!][FEATURE] Add compatibility for TYPO3 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'in2code.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.99.99'
+            'typo3' => '11.5.8-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
