d80eb99e739a09984ad0ea94b12380236346ae42
Alexander Kellner <alexander.kellner@in2code.de>
2023-05-26 14:21:08 +0200

    [SECURITY] Prevent possible SQL injection through IpAddress GET parameter
    Related: https://projekte.in2code.de/issues/57061

--- Classes/Domain/Service/IpToCountry/LocalDatabase.php
+++ Classes/Domain/Service/IpToCountry/LocalDatabase.php
@@ -44,9 +44,14 @@ class LocalDatabase extends AbstractIpToCountry implements IpToCountryInterface
     {
         $connection = DatabaseUtility::getConnectionForTable(self::TABLE_NAME);
         $sql = 'select countryCode from ' . self::TABLE_NAME
-            . ' where inet_aton("' . $ipAddress . '") >= inet_aton(ipRangeStart)' .
-            ' and inet_aton("' . $ipAddress . '") <= inet_aton(ipRangeEnd) limit 1';
+            . ' where inet_aton("' . $this->sanitizeIpAddress($ipAddress) . '") >= inet_aton(ipRangeStart)' .
+            ' and inet_aton("' . $this->sanitizeIpAddress($ipAddress) . '") <= inet_aton(ipRangeEnd) limit 1';
         $result = (string)$connection->query($sql)->fetchColumn(0);
         return strtolower($result);
+    }
+
+    protected function sanitizeIpAddress(string $ipAddress): string
+    {
+        return preg_replace('~[^0-9\.]~', '', $ipAddress);
     }
 }
