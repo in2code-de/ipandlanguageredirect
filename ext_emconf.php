6cced28b3e5d3843d79437395a6453712580002d
Andreas Nedbal <andreas.nedbal@in2code.de>
2025-01-13 14:44:21 +0100

    [TASK] Increase extension version constraints to include v13 (and remove v11)

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'in2code.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.8-12.4.99'
+            'typo3' => '12.4.0-13.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
